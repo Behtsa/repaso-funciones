9b9066019619d473b588290b367f63dcc067c8d6
Behtsa <monze_gdbm@hotmail.com>
2017-11-06 17:30:21 -0600

    Adding functionality

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,26 @@
 var firstDigit = parseFloat(prompt("Please enter first number"));
 var secondDigit = parseFloat(prompt("Please enter second number"));
+var operation = parseInt(prompt("Select the operation you want to do\n 1.Suma \n 2.Resta \n 3.Multiplicacion \n 4. Division"));
+
+function calculator(operation){
+	switch (operation) {
+		case 1:
+			sum(firstDigit,secondDigit);
+			break;
+		case 2: 
+			substraction(firstDigit, secondDigit);
+			break;
+		case 3:
+			multiply(firstDigit, secondDigit);
+			break;
+		case 4:
+			division(firstDigit, secondDigit);
+			break;
+		default:
+			alert("Ingresa una operacion valido");
+			break;
+	}
+}
 
 function sum(firstDigit, secondDigit) {
 	var result = firstDigit + secondDigit; 
@@ -21,7 +42,9 @@ function division(firstDigit, secondDigit) {
 	return document.write("El resultado de la division es: " + result + "<br>")
 }
 
-sum(firstDigit, secondDigit);
+calculator(operation);
+
+/*sum(firstDigit, secondDigit);
 substraction(firstDigit, secondDigit);
 multiply(firstDigit, secondDigit);
-division(firstDigit, secondDigit);
+division(firstDigit, secondDigit);*/
